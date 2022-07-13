24596c227f6edc367651266f4551aee1e152f145
Quentin Ikeno <ikenoq@gmail.com>
2022-07-12 18:46:24 -1000

    Remove commented out functional components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import React from "react";
 import JokeList from "./JokeList";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <JokeList />
-//     </div>
-//   );
-// }
-
 class App extends React.Component {
 	render() {
 		return (
